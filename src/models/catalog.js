d807a37eb1440a0b6120095b428719f2651539fc
GitYarisha <yarishaheera@u.nus.edu>
2018-08-30 01:40:00 +0800

    defaiult products

---
+++ src/models/catalog.js
@@ -0,0 +1,28 @@
+[
+    {
+        "_id": ObjectId("5b856ff7912a64363035e59c"),
+        "productId": 1234,
+        "productName": "IPhone",
+        "productCategory": "Mobile",
+        "productPrice": 1000,
+        "productDescription": "Apple Iphone",
+        "productImageUrl": "https://cdn2.gsmarena.com/vv/bigpic/apple-iphone-x.jpg",
+        "productAdded": 1535471027,
+        "productQuatity": 250,
+        "productSeller": "Flipkart",
+        "__v": 0
+    },
+    {
+        "_id": ObjectId("5b857379641c6b348c7a9d92"),
+        "productId": 12345,
+        "productName": "Oneplus 6",
+        "productCategory": "Mobile",
+        "productPrice": 699,
+        "productDescription": "Best Phone",
+        "productImageUrl": "https://cdn2.gsmarena.com/vv/bigpic/oneplus-6-.jpg",
+        "productAdded": 1535471028,
+        "productQuatity": 275,
+        "productSeller": "Amazon",
+        "__v": 0
+    }
+]
